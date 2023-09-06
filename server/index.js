078f464d159924ff005c9dfca204bc6b5ace4ac6
bilele123123 <tple06203@gmail.com>
2023-09-05 23:42:29 -0700

    Create api post request to save user cookie.

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.post('/login', async (req,res) => {
         const passOk = bcrypt.compareSync(password, foundUser.password);
         if (passOk) {
             jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
-                res.cookie('token', token).json({
+                res.cookie('token', token, { sameSite: 'none', secure: true }).json({
                     id: foundUser._id,
                 });
             });
