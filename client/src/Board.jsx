e3980a884930154b40018aa2bf0c8ec67424721c
bilele123123 <tple06203@gmail.com>
2023-09-20 23:48:59 -0700

    Add basic layout for main content.

--- client/src/Board.jsx
+++ client/src/Board.jsx
@@ -2,8 +2,24 @@ export default function Board() {
     return (
         <div className="flex justify-center h-screen bg-blue-100">
             <div className="flex flex-col w-screen items-center">
-                <div className="bg-red-100 w-3/4 h-20">Nav</div>
-                <div className="bg-blue-300 w-3/4 h-screen">Main content</div>
+                <div className="bg-blue-300 w-screen h-20">
+                    <div>Members Only</div>
+                    <div>
+                        <button className="bg-red-100 p-2 m-2">Log In</button>
+                        <button className="bg-red-100 p-2 m-2">Register</button>
+                    </div>
+                </div>
+                <div className="bg-blue-100 w-screen h-screen">
+                    <div>Main content</div>
+                    <div className="bg-red-100 h-1/4 w-1/3">
+                        Messages box
+                        <div>
+                            Poster info
+                            <div>User profile</div>
+                            <div>Date</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
