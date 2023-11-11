a78c7ad868de526c9f6c15f9b1bc46ee487c8445
bilele123123 <tple06203@gmail.com>
2023-11-10 23:57:07 -0800

    Create component to display error message for invalid userrname/existing.

--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -7,16 +7,32 @@ export default function RegisterAndLoginForm() {
     const [password, setPassword] = useState('');
     const [isLoginOrRegister, setIsLoginOrRegister] = useState('register');
     const [code, setCode] = useState('');
+    const [error, setError] = useState('');
     const { setUsername: setLoggedInUsername, setId } = useContext(UserContext);
 
     async function handleSubmit(ev) {
         ev.preventDefault();
         const url = isLoginOrRegister === 'register' ? 'register' : 'login';
         const dataToSend = isLoginOrRegister === 'register' ? { username, password, code } : { username, password };
-        const { data } = await axios.post(url, dataToSend, { headers: { 'Content-Type': 'application/json' } });
-        setLoggedInUsername(username);
-        setId(data.id);
+
+        try {
+            const { data } = await axios.post(url, dataToSend, { headers: { 'Content-Type': 'application/json' } });
+            setLoggedInUsername(username);
+            setId(data.id);
+        } catch (error) {
+            if (error.response.status === 400) {
+                setError('Username already taken');
+            } else if (error.response.status === 401) {
+                setError('Username not found/Password is incorrect');
+            } else {
+                setError('Unexpected error');
+            }
+        }
     }
+
+    const handleInputFocus = () => {
+        setError('');
+    };
 
     return (
         <div className="bg-blue-100 h-screen flex items-center">
@@ -26,30 +42,34 @@ export default function RegisterAndLoginForm() {
                     onChange={(ev) => setUsername(ev.target.value)}
                     type="text"
                     placeholder="username"
-                    className="block w-full rounded-sm p-2 mb-2 border"
+                    className={`block w-full rounded-sm p-2 mb-2 border ${error && 'border-red-500'}`}
+                    onFocus={handleInputFocus}
                     required
-                ></input>
+                />
                 <input
                     value={password}
                     onChange={(ev) => setPassword(ev.target.value)}
                     type="password"
                     placeholder="password"
-                    className="block w-full rounded-sm p-2 mb-2 border"
+                    className={`block w-full rounded-sm p-2 mb-2 border ${error && 'border-red-500'}`}
+                    onFocus={handleInputFocus}
                     required
-                ></input>
+                />
                 {isLoginOrRegister === 'register' && (
                     <input
                         value={code}
                         onChange={(ev) => setCode(ev.target.value)}
                         type="password"
                         placeholder="secret code"
-                        className="block w-full rounded-sm p-2 mb-2 border"
-                    ></input>
+                        className={`block w-full rounded-sm p-2 mb-2 border ${error && 'border-red-500'}`}
+                        onFocus={handleInputFocus}
+                    />
                 )}
                 <button className="bg-blue-500 text-white block w-full rounded-sm p-1">
                     {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
                 </button>
-                <div className="text-center mt-2">
+                <div className="text-center mt-2 mb-2">
+                    {error && <div className="text-red-500">{error}</div>}
                     <div>
                         {isLoginOrRegister === 'register' ? (
                             <div>
