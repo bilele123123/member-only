fa68f612aab103cc8111afec6544595cd0ea575c
bilele123123 <tple06203@gmail.com>
2023-11-11 23:21:23 -0800

    Include user role in server response and set it in state.

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -6,14 +6,17 @@ export const UserContext = createContext({});
 export function UserContextProvider({children}) {
     const [username, setUsername] = useState(null);
     const [id, setId] = useState(null);
+    const [role, setRole] = useState(null);
+
     useEffect(() => {
         axios.get('/profile').then(response => {
             setId(response.data.userId);
             setUsername(response.data.username);
+            setRole(response.data.role);
         });
     }, []);
     return (
-        <UserContext.Provider value={{username, setUsername, id, setId}}>
+        <UserContext.Provider value={{username, setUsername, id, setId, role}}>
             {children}
         </UserContext.Provider>
     );
