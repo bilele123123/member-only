034c4e537e2eb3cbf4c9172a0a972ce0672d6a3a
bilele123123 <tple06203@gmail.com>
2023-11-11 23:20:23 -0800

    Defined user role in user schema.

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
     username: {type: String, unique: true}, 
     password: String,
     code: String,
+    role: { type: String, default: 'user' },
 }, {timestamps: true});
 
 const User = mongoose.model('User', UserSchema);
